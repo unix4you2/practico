87a5f975931fd0ff4bd37a1e7892b64905e9b34b
unix4you2 <unix4you2@gmail.com>
2012-07-10 13:49:40 -0500

    Se elimina la impresión de la acción al ser vacía.
    En el archivo marco_abajo no se muestra ahora el valor de la vble acciòn cuando apenas se inicia el aplicativo.

--- core/marco_abajo.php
+++ core/marco_abajo.php
@@ -3,7 +3,10 @@
 	<tr><td>
 		<table width="100%" cellspacing="0" cellpadding="0" border=0 class="MarcoInferior"><tr>
 			<td align="left" valign="bottom" width="50%">
-				&nbsp;&nbsp;Instante:&nbsp;&nbsp;<?php echo $fecha_operacion_guiones;?>&nbsp;&nbsp;<?php echo $hora_operacion_puntos;?><?php if($Id_usuario="admin") echo " - <font color=yellow>Accion: ".$accion."</font>"; ?>
+				&nbsp;&nbsp;Instante:&nbsp;&nbsp;<?php echo $fecha_operacion_guiones;?>&nbsp;&nbsp;<?php echo $hora_operacion_puntos;?>
+				<?php 
+				// Muestra la accion actual si el usuario es administrador y la accion no es vacia - Sirve como guia a la hora de crear objetos
+				if($Id_usuario="admin" && $accion!="") echo " - <font color=yellow>Accion: ".$accion."</font>"; ?>
 			</td>
 			<td align="right" valign="bottom" width="50%">
 				Software por John F. Arroyave Gutierrez - Proyecto de maestr&iacute;a UNAB&nbsp;&nbsp;
