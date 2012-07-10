0c38ee500c2e66d4be60702c38ef43c3e162901f
unix4you2 <unix4you2@gmail.com>
2012-07-10 18:17:20 -0500

    Depuracion de codigo sobre tablas.php Se elimina codigo pendiente por depurarse.  Es pasado a un archivo de control para revision posterior.

--- core/tablas.php
+++ core/tablas.php
@@ -15,42 +15,6 @@
 ?>
 
 
-
-
-
-
-<?php
-/*
-// VARIABLES DE CONEXION
-$server = "localhost";
-$dbuser = "usuario";
-$dbpassword = "password";
-$dbname = "nombre_base_de_datos"; 
-  $db = mysql_connect($dbhost, $dbuser, $dbpassword) or die("Connection Error: " . mysql_error());
-mysql_select_db($dbname) or die("Error al conectar a la base de datos.");
-  //EXRAEMOS LAS TABLAS DE LA BASE DE DATOS
-$sql = "SHOW TABLES";
-$tablas = mysql_query( $sql) or die("No se puede ejecutar la consulta: ".mysql_error());
-while ($tabla = mysql_fetch_assoc($tablas))  {
-	foreach ($tabla as $item => $nombre_tabla) {	
-		echo $nombre_tabla.": ";
-		//OPTIMIZAMOS LAS TABLAS
-		mysql_query("OPTIMIZE TABLE ".$nombre_tabla) or die("No se puede ejecutar la consulta: ".mysql_error());
-		// MOSTRAMOS EL RESULTADO
-		if (mysql_errno()){
-			echo " No ha podido ser optimizada.";
-		}else{
-			echo "Optimizada.";
-		}
-		echo "<br />";
-	}
-}
-mysql_close($db);
-* */
-?>
-
-
-
 <!--  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ -->
 <?php
 	if ($accion=="eliminar_campo")
