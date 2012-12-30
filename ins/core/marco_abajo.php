f3d42cf09cef16a7f2befa3f982fd3303a3d11e5
unix4you2 <unix4you2@gmail.com>
2012-12-30 12:13:00 -0500

    Inclusión de encabezados de licencia GPL 2.0 a todos los fuentes

--- ins/core/marco_abajo.php
+++ ins/core/marco_abajo.php
@@ -1,5 +1,26 @@
-	<!-- INICIO DEL MENU INFERIOR -->	
+<?php
+	/*
+	Copyright (C) 2013  John F. Arroyave Gutiérrez
+						unix4you2@gmail.com
 
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+	*/
+?>
+
+
+	<!-- INICIO DEL MENU INFERIOR -->	
 	<tr><td>
 		<table width="100%" cellspacing="0" cellpadding="0" border=0 class="MarcoInferior"><tr>
 			<td align="left" valign="bottom" width="50%">
