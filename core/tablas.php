fceb947414220eca25478981f1af8ef0285ee33f
John Arroyave <unix4you2@gmail.com>
2014-11-21 15:51:32 -0500

    Opciones de tipo de dato específicas para MySQL y MariaDB

--- core/tablas.php
+++ core/tablas.php
@@ -197,10 +197,67 @@ if ($accion=="editar_tabla")
 								<option value="VARCHAR"><?php echo $MULTILANG_TblCadena; ?></option>
 								<option value="TEXT"><?php echo $MULTILANG_TblTexto; ?></option>
 								<option value="DATE"><?php echo $MULTILANG_TblFecha; ?></option>
-							<!--
-							 OPCIONES PREVIAS: SOLO MYSQL
-								<option value="INT">Entero</option><option value="VARCHAR">Cadena (Hasta 255)</option><option value="TEXT">Texto (Ilimitado)</option><option value="DATE">Fecha (sin hora)</option><optgroup label="Tipos numericos comunes"><option value="TINYINT">TINYINT</option><option value="SMALLINT">SMALLINT</option><option value="MEDIUMINT">MEDIUMINT</option><option value="INT">INT</option><option value="BIGINT">BIGINT</option><option value="-">-</option><option value="DECIMAL">DECIMAL</option><option value="FLOAT">FLOAT</option><option value="DOUBLE">DOUBLE</option><option value="REAL">REAL</option><option value="-">-</option><option value="BIT">BIT</option><option value="BOOLEAN">BOOLEAN</option><option value="SERIAL">SERIAL</option></optgroup><optgroup label="Tipos de Fecha y Hora"><option value="DATE">DATE</option><option value="DATETIME">DATETIME</option><option value="TIMESTAMP">TIMESTAMP</option><option value="TIME">TIME</option><option value="YEAR">YEAR</option></optgroup><optgroup label="Tipos de cadenas de caracteres"><option value="CHAR">CHAR</option><option value="VARCHAR">VARCHAR</option><option value="-">-</option><option value="TINYTEXT">TINYTEXT</option><option value="TEXT">TEXT</option><option value="MEDIUMTEXT">MEDIUMTEXT</option><option value="LONGTEXT">LONGTEXT</option><option value="-">-</option><option value="BINARY">BINARY</option><option value="VARBINARY">VARBINARY</option><option value="-">-</option><option value="TINYBLOB">TINYBLOB</option><option value="MEDIUMBLOB">MEDIUMBLOB</option><option value="BLOB">BLOB</option><option value="LONGBLOB">LONGBLOB</option><option value="-">-</option><option value="ENUM">ENUM</option><option value="SET">SET</option></optgroup><optgroup label="Tipos espaciales"><option value="GEOMETRY">GEOMETRY</option><option value="POINT">POINT</option><option value="LINESTRING">LINESTRING</option><option value="POLYGON">POLYGON</option><option value="MULTIPOINT">MULTIPOINT</option><option value="MULTILINESTRING">MULTILINESTRING</option><option value="MULTIPOLYGON">MULTIPOLYGON</option><option value="GEOMETRYCOLLECTION">GEOMETRYCOLLECTION</option></optgroup>
-							-->
+
+								<?php
+									//Verifica si se trata de un motor MySQL o compatible y agrega los tipos especificos
+									if ($MotorBD=="mysql")
+										{
+											echo '
+											<optgroup label="Tipos numericos comunes">
+												<option value="TINYINT">TINYINT</option>
+												<option value="SMALLINT">SMALLINT</option>
+												<option value="MEDIUMINT">MEDIUMINT</option>
+												<option value="INT">INT</option>
+												<option value="BIGINT">BIGINT</option>
+												<option value="-">-</option>
+												<option value="DECIMAL">DECIMAL</option>
+												<option value="FLOAT">FLOAT</option>
+												<option value="DOUBLE">DOUBLE</option>
+												<option value="REAL">REAL</option>
+												<option value="-">-</option>
+												<option value="BIT">BIT</option>
+												<option value="BOOLEAN">BOOLEAN</option>
+												<option value="SERIAL">SERIAL</option>
+											</optgroup>
+											<optgroup label="Tipos de Fecha y Hora">
+												<option value="DATE">DATE</option>
+												<option value="DATETIME">DATETIME</option>
+												<option value="TIMESTAMP">TIMESTAMP</option>
+												<option value="TIME">TIME</option>
+												<option value="YEAR">YEAR</option>
+											</optgroup>
+											<optgroup label="Tipos de cadenas de caracteres">
+												<option value="CHAR">CHAR</option>
+												<option value="VARCHAR">VARCHAR</option>
+												<option value="-">-</option>
+												<option value="TINYTEXT">TINYTEXT</option>
+												<option value="TEXT">TEXT</option>
+												<option value="MEDIUMTEXT">MEDIUMTEXT</option>
+												<option value="LONGTEXT">LONGTEXT</option>
+												<option value="-">-</option>
+												<option value="BINARY">BINARY</option>
+												<option value="VARBINARY">VARBINARY</option>
+												<option value="-">-</option>
+												<option value="TINYBLOB">TINYBLOB</option>
+												<option value="MEDIUMBLOB">MEDIUMBLOB</option>
+												<option value="BLOB">BLOB</option>
+												<option value="LONGBLOB">LONGBLOB</option>
+												<option value="-">-</option>
+												<option value="ENUM">ENUM</option>
+												<option value="SET">SET</option>
+											</optgroup>
+											<optgroup label="Tipos espaciales">
+												<option value="GEOMETRY">GEOMETRY</option>
+												<option value="POINT">POINT</option>
+												<option value="LINESTRING">LINESTRING</option>
+												<option value="POLYGON">POLYGON</option>
+												<option value="MULTIPOINT">MULTIPOINT</option>
+												<option value="MULTILINESTRING">MULTILINESTRING</option>
+												<option value="MULTIPOLYGON">MULTIPOLYGON</option>
+												<option value="GEOMETRYCOLLECTION">GEOMETRYCOLLECTION</option>
+											</optgroup>';
+										}
+								?>
 							</select>
 						</td>
 					</tr>
