9a3c0610020789c48d2dd63164145abf3df8628d
John Arroyave <unix4you2@gmail.com>
2014-12-02 10:28:29 -0500

    Pruebas Travis

---
+++ dev_tools/tests/t_archivos.php
@@ -0,0 +1,41 @@
+<?php
+	/*
+	Copyright (C) 2013  John F. Arroyave Gutiérrez
+						unix4you2@gmail.com
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+	<pre>
+	<b>Importante: Si usted esta visualizando este mensaje en su navegador,
+	entonces PHP no esta instalado correctamente en su servidor web!</b>
+	</pre>
+	*/
+
+	/*
+		Title: t_basedatos
+		Ubicacion *[dev_tools/tests/t_archivos.php]*.  Pruebas para evaluacion de rutas y archivos basicos necesarios
+	*/
+
+	// Definicion de variables para almacenar resultado
+	$estado_final="0";
+
+
+	//if (!file_exists("core/configuracion.php")) $estado_final="1";
+
+
+
+
+	// Devuelve resultado final de las pruebas
+	return $estado_final;
