75ec7525e9b24e5391f3590e8907483072960c73
John Arroyave <unix4you2@gmail.com>
2015-01-27 15:40:44 -0500

    Documentación

---
+++ dev_tools/docs/variables.php
@@ -0,0 +1,47 @@
+<?php
+	/*
+	Copyright (C) 2013  John F. Arroyave Gutiérrez
+						unix4you2@gmail.com
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+	*/
+
+    /*
+    Title: Documentacion para desarrolladores
+    Ubicacion *[/dev_tools/docs/variables.php]*.  Archivo con documentacion general de variables
+
+
+    Section: Variables de sesion
+    Lista de variables de sesion normalmente disponibles en tiempo de ejecucion
+
+    Variables de entrada:
+
+        $PCOSESS_LoginUsuario - Contiene el UID unico del usuario que se encuentra logueado en el sistema
+        $PCOSESS_SesionAbierta - Determina si la sesion se ha iniciado correctamente (0=No iniciada, 1=Iniciada)
+
+    Section: Variables generales (Globales)
+    Las variables se encuentran disponibles en el entorno global para ser utilizadas en diferentes funciones
+
+        $PCO_ValorBusquedaBD - 
+        $PCO_CampoBusquedaBD - 
+        $PCO_ErrorTitulo - 
+        $PCO_ErrorDescripcion - 
+        $PCO_FechaOperacionGuiones - 
+        $PCO_HoraOperacionPuntos - 
+        $PCO_HoraOperacion - 
+        $PCO_FechaOperacion - 
+
+    */
+
