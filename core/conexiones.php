af4a357276f5519df9bed4c8f1328d95d7a8f41d
John Arroyave <unix4you2@gmail.com>
2015-03-03 14:46:56 -0500

    Mejora a errores cuando BD no existe

--- core/conexiones.php
+++ core/conexiones.php
@@ -112,8 +112,9 @@
 		}
 	catch( PDOException $ErrorPDO)
 		{
-            $mensaje_final="Error de conexion con la base de datos.<br>Verifique los valores existentes en el archivo <b>configuracion.php</b> y la disponibilidad de PDO y modulos asociados a su motor de bases de datos en su <b>php.ini</b>.  <br><b>Detalles:</b> ".$ErrorPDO->getMessage()."";
-			mensaje('<i class="fa fa-warning fa-2x text-danger texto-blink"></i> ', $mensaje_final, '', '', 'alert alert-danger alert-dismissible');
+            include_once("core/comunes.php"); //Incluye la libreria de base al menos para presentar mensaje de error
+            $mensaje_final="Error de conexion con la base de datos. <hr>Verifique los valores existentes en el archivo <b>configuracion.php</b> y la disponibilidad de PDO y modulos asociados a su motor de bases de datos en su <b>php.ini</b>.";
+			mensaje('<i class="fa fa-warning fa-3x text-danger texto-blink"></i> '.$mensaje_final, "<li><b>Detalles:</b> ".$ErrorPDO->getMessage(), '', '', 'alert alert-danger alert-dismissible');
 			$hay_error=1; //usada globalmente durante el proceso de instalacion
 		}
 
