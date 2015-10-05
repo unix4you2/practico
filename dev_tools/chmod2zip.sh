34fcfddf3fdbef95093b915ac71094eeb293794e
unix4you2 <unix4you2@gmail.com>
2015-10-05 15:24:44 -0500

    Permisos para el demo

--- dev_tools/chmod2zip.sh
+++ dev_tools/chmod2zip.sh
@@ -36,7 +36,7 @@
     chmod 777 core
     chmod 777 tmp
     chmod 777 ins
-    chmod 777 mod/pcoder/demo.php
+    chmod 777 mod/pcoder/demos/*
     chmod -R 777 mod/pcc_erp/*
     
     #Permisos de archivos basicos de configuracion
