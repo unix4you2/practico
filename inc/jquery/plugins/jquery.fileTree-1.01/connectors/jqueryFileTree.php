8b01d5d9e53af6f57cfe1f77ae0ad5d430d88cd1
unix4you2 <unix4you2@gmail.com>
2016-02-05 09:35:00 -0500

    Actualización plugin JQueryFileTree

--- inc/jquery/plugins/jquery.fileTree-1.01/connectors/jqueryFileTree.php
+++ inc/jquery/plugins/jquery.fileTree-1.01/connectors/jqueryFileTree.php
@@ -26,14 +26,17 @@ if( file_exists($root . $_POST['dir']) ) {
 		// All dirs
 		foreach( $files as $file ) {
 			if( file_exists($root . $_POST['dir'] . $file) && $file != '.' && $file != '..' && is_dir($root . $_POST['dir'] . $file) ) {
-				echo "<li class=\"directory collapsed\"><a rel=\"" . htmlentities($_POST['dir'] . $file) . "/\">" . htmlentities($file) . "</a></li>";
+				echo "<li class=\"directory collapsed\"><a OnClick=\"UltimaCarpetaSeleccionada='" . htmlentities($_POST['dir'] . $file) . "'; UltimoArchivoSeleccionado=''; \" rel=\"" . htmlentities($_POST['dir'] . $file) . "/\">" . htmlentities($file) . "</a></li>";
 			}
 		}
-		// All files
-		foreach( $files as $file ) {
-			if( file_exists($root . $_POST['dir'] . $file) && $file != '.' && $file != '..' && !is_dir($root . $_POST['dir'] . $file) ) {
-				$ext = preg_replace('/^.*\./', '', $file);
-				echo "<li class=\"file ext_$ext\"><a rel=\"" . htmlentities($_POST['dir'] . $file) . "\">" . htmlentities($file) . "</a></li>";
+		// All files, only if not hidden flag is received
+		if($_GET['nofiles']!="true")
+		{
+			foreach( $files as $file ) {
+				if( file_exists($root . $_POST['dir'] . $file) && $file != '.' && $file != '..' && !is_dir($root . $_POST['dir'] . $file) ) {
+					$ext = preg_replace('/^.*\./', '', $file);
+					echo "<li class=\"file ext_$ext\"><a OnClick=\"UltimoArchivoSeleccionado='" . htmlentities($_POST['dir'] . $file) . "'; UltimaCarpetaSeleccionada=''; \" rel=\"" . htmlentities($_POST['dir'] . $file) . "\">" . htmlentities($file) . "</a></li>";
+				}
 			}
 		}
 		echo "</ul>";	
