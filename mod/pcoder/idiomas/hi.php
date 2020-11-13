48cc213997d9464a6b4e4d99cab87d2eaceaf653
unix4you2 <unix4you2@gmail.com>
2020-11-13 19:39:28 +0000

    Cambio de licencia GNU/GPLv2 hacia BSD Simplificada

--- mod/pcoder/idiomas/hi.php
+++ mod/pcoder/idiomas/hi.php
@@ -1,26 +1,32 @@
 <?php
 	/*
-	   PCODER (Editor de Codigo en la Nube)
-	   Sistema de Edicion de Codigo basado en PHP
-	   Copyright (C) 2013  John F. Arroyave Gutiérrez
-						   unix4you2@gmail.com
-						   www.practico.org
-
-	 This program is free software: you can redistribute it and/or modify
-	 it under the terms of the GNU General Public License as published by
-	 the Free Software Foundation, either version 3 of the License, or
-	 (at your option) any later version.
-
-	 This program is distributed in the hope that it will be useful,
-	 but WITHOUT ANY WARRANTY; without even the implied warranty of
-	 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	 GNU General Public License for more details.
-
-	 You should have received a copy of the GNU General Public License
-	 along with this program.  If not, see <http://www.gnu.org/licenses/>
-	*/
-
-	/*
+	 _
+	|_) _ _  _ _|_. _ _					  	Copyright (C) 2020
+	|  | (_|(_  | |(_(_) 				  	John F. Arroyave Gutiérrez
+	  www.practico.org					  	unix4you2@gmail.com
+                                            All rights reserved.
+    
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+    
+    1. Redistributions of source code must retain the above copyright notice, this
+       list of conditions and the following disclaimer.
+    
+    2. Redistributions in binary form must reproduce the above copyright notice,
+       this list of conditions and the following disclaimer in the documentation
+       and/or other materials provided with the distribution.
+    
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+    AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+    IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+    FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+    DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+    SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+    CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+    OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+    
 		Title: Idioma hindi para modulo de PBrowser
 		Ubicacion *[/inc/idioma/hi.php]*.  Incluye la definicion de variables utilizadas para presentar mensajes en el idioma correspondiente
 		NOTAS IMPORTANTES:
@@ -106,7 +112,6 @@
 	$MULTILANG_PCODER_ErrorRW='आप इस फ़ाइल को लिखने के लिए अधिकार नहीं है! कोई भी परिवर्तन खो जाएगा!';
 	$MULTILANG_PCODER_SaltarLinea='लाइन के लिए कूदो';
 	$MULTILANG_PCODER_Acerca='के बारे में';
-	$MULTILANG_PCODER_ResumenLicencia='यह उपकरण के तहत नि: शुल्क सॉफ्टवेयर है GNU-GPL v3 License';
 	$MULTILANG_PCODER_AparienciaEditor='संपादक विषय';
 	$MULTILANG_PCODER_TamanoFuente='फ़ॉन्ट आकार';
 	$MULTILANG_PCODER_LenguajeProg='प्रोग्रामिंग भाषा';
