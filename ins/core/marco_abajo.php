24029454c9116c32fee7f5ea30af09a02dc69dc2
unix4you2 <unix4you2@gmail.com>
2022-02-14 19:31:36 +0000

    Instalador nuevo JQuery

--- ins/core/marco_abajo.php
+++ ins/core/marco_abajo.php
@@ -42,7 +42,7 @@
     <!-- /#wrapper inicial -->
 
     <!-- jQuery -->
-	<script type="text/javascript" src="../inc/jquery/jquery-2.1.0.min.js"></script>
+	<script type="text/javascript" src="../inc/jquery/jquery-2.2.4.min.js"></script>
     <!-- Bootstrap Core JavaScript -->
     <script type="text/javascript" src="../inc/bootstrap/js/bootstrap.min.js"></script>
 
