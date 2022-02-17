c07013bbd9cdcf97a79e2d736da07ac68254d0a5
unix4you2 <unix4you2@gmail.com>
2022-02-17 14:11:31 +0000

    Retorno de licencia BSD2-GPL3

--- dev_tools/practico2zip.sh
+++ dev_tools/practico2zip.sh
@@ -1,31 +1,45 @@
 #!/bin/bash
+#	 _
+#	|_) _ _  _ _|_. _ _					  	Copyright (C) 2012-2022
+#	|  | (_|(_  | |(_(_) 				  	John F. Arroyave Gutiérrez
+#	  www.practico.org					  	unix4you2@gmail.com
+#                                            All rights reserved.
+#    
+#	 This program is free software: you can redistribute it and/or modify
+#	 it under the terms of the GNU General Public License as published by
+#	 the Free Software Foundation, either version 3 of the License, or
+#	 (at your option) any later version.
+#
+#	 This program is distributed in the hope that it will be useful,
+#	 but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	 GNU General Public License for more details.
+#
+#	 You should have received a copy of the GNU General Public License
+#	 along with this program.  If not, see <http://www.gnu.org/licenses/>
+#	 
+#	            --- TRADUCCION NO OFICIAL DE LA LICENCIA ---
+#
+#     Esta es una traducción no oficial de la Licencia Pública General de
+#     GNU al español. No ha sido publicada por la Free Software Foundation
+#     y no establece los términos jurídicos de distribución del software 
+#     publicado bajo la GPL 3 de GNU, solo la GPL de GNU original en inglés
+#     lo hace. De todos modos, esperamos que esta traducción ayude a los
+#     hispanohablantes a comprender mejor la GPL de GNU:
+#	 
+#     Este programa es software libre: puede redistribuirlo y/o modificarlo
+#     bajo los términos de la Licencia General Pública de GNU publicada por
+#     la Free Software Foundation, ya sea la versión 3 de la Licencia, o 
+#     (a su elección) cualquier versión posterior.
+#
+#     Este programa se distribuye con la esperanza de que sea útil pero SIN
+#     NINGUNA GARANTÍA; incluso sin la garantía implícita de MERCANTIBILIDAD
+#     o CALIFICADA PARA UN PROPÓSITO EN PARTICULAR. Vea la Licencia General
+#     Pública de GNU para más detalles.
+#
+#     Usted ha debido de recibir una copia de la Licencia General Pública de
+#     GNU junto con este programa. Si no, vea <http://www.gnu.org/licenses/>
 
-#    _
-#   |_) _ _  _ _|_. _ _					  	Copyright (C) 2020
-#   |  | (_|(_  | |(_(_) 				  	John F. Arroyave Gutiérrez
-#     www.practico.org					  	unix4you2@gmail.com
-#                                           All rights reserved.
-#   
-#   Redistribution and use in source and binary forms, with or without
-#   modification, are permitted provided that the following conditions are met:
-#   
-#   1. Redistributions of source code must retain the above copyright notice, this
-#      list of conditions and the following disclaimer.
-#   
-#   2. Redistributions in binary form must reproduce the above copyright notice,
-#      this list of conditions and the following disclaimer in the documentation
-#      and/or other materials provided with the distribution.
-#   
-#   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-#   AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-#   IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-#   DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-#   FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-#   DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-#   SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-#   CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-#   OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-#   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #Genera algunos elementos internos
 php objeto2xml.php
