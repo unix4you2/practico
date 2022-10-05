792b8a3f121ea924b735cc55393d8b7e976e80d4
unix4you2 <unix4you2@gmail.com>
2022-10-05 12:18:07 +0000

    Actions

---
+++ dev/test/ga_basedatos.php
@@ -0,0 +1,75 @@
+<?php
+/*
+	 _
+	|_) _ _  _ _|_. _ _					  	Copyright (C) 2012-2022
+	|  | (_|(_  | |(_(_) 				  	John F. Arroyave Gutiérrez
+	  www.practico.org					  	unix4you2@gmail.com
+                                            All rights reserved.
+    
+	====================================
+	   Set de pruebas: CodeQL y Otros
+	====================================
+	DESCRIPCION:    Archivo de ejecucion de script para generacion de base de datos sobre el servidor
+	CODIGO QL:      php /var/www/html/practico/dev/test/ga_basedatos.php
+	                curl http://localhost/practico/dev/test/ga_basedatos.php
+
+*/
+
+	// Ejecuta los scripts de creacion de la BD si se requiere
+	$total_ejecutadas=0;
+
+			include_once("../../core/configuracion.php");
+			include_once("../../core/conexiones.php");
+			include_once("../../core/comunes.php");
+			
+			//Abre el archivo con los queries dependiendo del motor
+			$RutaScriptSQL="../../ins/sql/practico.mysql";
+			
+			echo ">>> Ejecutando SCRIPTS en ".$RutaScriptSQL;
+			
+			$archivo_consultas=fopen($RutaScriptSQL,"r");
+			$total_consultas= fread($archivo_consultas,filesize($RutaScriptSQL));
+			fclose($archivo_consultas);
+ 
+        	echo ">>> Volcado de SCRIPTS: ".$total_consultas;
+
+ 
+			$arreglo_consultas = PCO_SegmentarSQL($total_consultas);
+			foreach($arreglo_consultas as $consulta)
+				{
+					try
+						{
+							//Cambia el prefijo predeterminado en caso que haya sido personalizado en la instalacion
+							$consulta=str_replace("core_",$TablasCore,$consulta);
+							//Cambia parametros iniciales de aplicacion
+							$consulta=str_replace("PAR_NombreCortoEmpresa",$NombreCortoEmpresa,$consulta);
+							$consulta=str_replace("PAR_NombreAplicacion",$NombreAplicacion,$consulta);
+							$consulta=str_replace("PAR_VersionAplicacion",$VersionAplicacion,$consulta);
+
+							//Ejecuta el query
+							$consulta_enviar = $ConexionPDO->prepare($consulta);
+							$consulta_enviar->execute();
+							$total_ejecutadas++;
+						}
+					catch( PDOException $ErrorPDO)
+						{
+							echo "<hr><b><font color=red>".$MULTILANG_Atencion."!!!: </font>".$MULTILANG_ErrorScripts.". SQL: ".$consulta." ".$MULTILANG_Error.": ".$ErrorPDO->getMessage()."</b>";
+							$hay_error=1; //usada globalmente durante el proceso de instalacion
+						}
+				}
+
+			//Actualiza las llaves de paso de los usuarios insertados
+			$LlaveEnMD5=hash("md5", $LlaveDePaso);
+			$consulta="UPDATE ".$TablasCore."usuario SET llave_paso='".$LlaveEnMD5."' ";
+			$consulta_enviar = $ConexionPDO->prepare($consulta);
+			$consulta_enviar->execute();
+
+    echo ">>> Total consultas ejecutadas: ".$total_ejecutadas;
+	
+	if ($hay_error)
+		{
+            echo ">>> Se encontraron ERRORES al ejecutar: ";
+            exit(1); //Finaliza con error
+		}
+	exit(0);
+?>
---
+++ dev/test/ga_configuracion.php
@@ -0,0 +1,66 @@
+<?php
+/*
+	 _
+	|_) _ _  _ _|_. _ _					  	Copyright (C) 2012-2022
+	|  | (_|(_  | |(_(_) 				  	John F. Arroyave Gutiérrez
+	  www.practico.org					  	unix4you2@gmail.com
+                                            All rights reserved.
+    
+	====================================
+	   Set de pruebas: CodeQL y Otros
+	====================================
+	DESCRIPCION:    Archivo de configuracion usado como origen en instalaciones automatizadas en entornos de prueba
+	CODIGO QL:      cp /var/www/html/practico/dev/test/ga_configuracion.php /var/www/html/practico/configuracion.php
+	
+*/
+$ServidorBD='localhost';
+$BaseDatos='practico';
+$UsuarioBD='root';
+$PasswordBD='root';
+$MotorBD='mysql';
+$PuertoBD='';
+$NombreRAD='Practico';
+$ArchivoCORE='';
+$TablasCore='core_';
+$TablasApp='app_';
+$LlaveDePaso='H76T9QFT7P';
+$ModoDepuracion=0;
+$PermitirReporteBugs=1;
+$DepuracionSQL=0;
+$BuscarActualizaciones=0;
+$ZonaHoraria='America/Bogota';
+$IdiomaPredeterminado='es';
+$IdiomaEnLogin=1;
+$Tema_PracticoFramework='bootstrap';
+$PCO_ArchivoImagenFondo='';
+$PCO_TransformacionColores='';
+$PCO_PermitirUsuariosModoNoche='1';
+$TipoCaptchaLogin='visual';
+$CaracteresCaptcha=4;
+$CodigoGoogleAnalytics='G-ZKZP142B9V';
+$Auth_TipoMotor='practico';
+$Auth_ProtoTransporte='';
+$Auth_PermitirReseteoClaves='0';
+$Auth_PermitirAutoRegistro='0';
+$Auth_PlantillaAutoRegistro='';
+$Auth_PresentarOauthInicio='0';
+$Auth_TipoEncripcion='plano';
+$Auth_LDAPServidor='172.29.196.181';
+$Auth_LDAPPuerto='3269';
+$Auth_LDAPDominio='practico.org';
+$Auth_LDAPOU='GrupoGenerico';
+$Activar_ModuloChat=0;
+$PWA_Activa=1;
+$PWA_DireccionTexto='auto';
+$PWA_Display='fullscreen';
+$PWA_Orientacion='portrait';
+$PWA_FCMSenderID='103953800507';
+$PWA_Scope='';
+$PWA_AutorizacionGPS='0';
+$PWA_AutorizacionFCM='0';
+$PWA_AutorizacionCAM='1';
+$PWA_AutorizacionMIC='0';
+$PWA_OcultarBarrasHerramientas='0';
+$_SeparadorCampos_='||_||';
+$ModoDesarrolladorPractico=0;
+$PCOVAR_Administradores='admin,john.arroyave';
