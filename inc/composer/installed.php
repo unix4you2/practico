36ff2b1d2475623a624dff7cbd206215bfb96022
unix4you2 <unix4you2@gmail.com>
2022-11-02 17:09:30 +0000

    Jquery como componente

---
+++ inc/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'practico/framework',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'fda75f3c15f3b692807b5eeacd471a8878e9a49d',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'components/jquery' => array(
+            'pretty_version' => '3.6.0',
+            'version' => '3.6.0.0',
+            'reference' => '6cf38ee1fd04b6adf8e7dda161283aa35be818c3',
+            'type' => 'component',
+            'install_path' => __DIR__ . '/../components/jquery',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'practico/framework' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'fda75f3c15f3b692807b5eeacd471a8878e9a49d',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
