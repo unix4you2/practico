b6d5ab910ecc000e2cf7daea4f1b816ad2d46fce
unix4you2 <unix4you2@gmail.com>
2022-11-02 11:53:57 +0000

    Transicion Travis

--- dev/test/ga_basedatos.php
+++ dev/test/ga_basedatos.php
@@ -78,7 +78,155 @@
 			$consulta_enviar->execute();
 
     echo ">>> Total consultas ejecutadas: ".$total_ejecutadas;
-	
+
+/*
+Script en transicion desde t_basedatos.php (Anteriormente Travis)
+
+	// Definicion de variables para almacenar resultado
+	$estado_final="0";
+	include_once("dev/test/z_consola.php");
+	include_once("core/comunes.php");
+	$accion="";
+	$hay_error=0;
+
+    //Define un arreglo con los motores a probar
+    $MotoresPruebas=array("mysql"); // Deprecated: "pgsql", "sqlite"
+
+    //Recorre el arreglo de motores y ejecuta el script en cada uno
+    foreach ($MotoresPruebas as $MotorEvaluado)
+        {
+            //Presenta estado sobre TravisCI
+            PCOCLI_MensajeSimple(" Pruebas sobre motor ".$MotorEvaluado);
+            if ($MotorEvaluado=="mysql")
+                {
+                    $ServidorBD='127.0.0.1';
+                    $BaseDatos='practico';
+                    $UsuarioBD='root';
+                    $PasswordBD='';
+                    $MotorBD=$MotorEvaluado;
+                    $PuertoBD='';
+                    $TablasCore='core_';
+                    $TablasApp='app_';
+                    $ConsultaVersionMotor="SELECT VERSION()";
+                }
+            if ($MotorEvaluado=="pgsql")
+                {
+                    $ServidorBD='127.0.0.1';
+                    $BaseDatos='practico';
+                    $UsuarioBD='postgres';
+                    $PasswordBD='';
+                    $MotorBD=$MotorEvaluado;
+                    $PuertoBD='';
+                    $TablasCore='core_';
+                    $TablasApp='app_';
+                    $ConsultaVersionMotor="SELECT version()";
+                }
+            if ($MotorEvaluado=="sqlite")
+                {
+                    //$ServidorBD='127.0.0.1';
+                    $BaseDatos='practico.db';
+                    //$UsuarioBD='root';
+                    //$PasswordBD='';
+                    $MotorBD=$MotorEvaluado;
+                    //$PuertoBD='';
+                    $TablasCore='core_';
+                    $TablasApp='app_';
+                    $ConsultaVersionMotor="select sqlite_version()";
+                }            //Recarga archivo de conexiones para reescribir variables de conexion y redefine la misma justo despues segun el motor
+            include_once("core/conexiones.php");
+            $ConexionPDO=PCO_NuevaConexionBD($MotorBD,$PuertoBD,$BaseDatos,$ServidorBD,$UsuarioBD,$PasswordBD);
+
+            $RegistroVersionMotor=PCO_EjecutarSQL($ConsultaVersionMotor)->fetch();
+            echo "\n\r";
+            echo "  VERSION: ".$RegistroVersionMotor[0];
+
+
+        	//PASO 1: Agrega las tablas y ejecuta consultas iniciales sobre la base de datos
+        		$total_ejecutadas=0;
+        		//Abre el archivo con los queries dependiendo del motor
+        		$RutaScriptSQL="ins/sql/practico.".$MotorEvaluado;
+        		
+        		$archivo_consultas=fopen($RutaScriptSQL,"r");
+        		$total_consultas= fread($archivo_consultas,filesize($RutaScriptSQL));
+        		fclose($archivo_consultas);
+        		$arreglo_consultas = PCO_SegmentarSQL($total_consultas);
+        		foreach($arreglo_consultas as $consulta)
+        			{
+        				try
+        					{
+        						//Ejecuta el query
+        						$consulta_enviar = $ConexionPDO->prepare($consulta);
+        						$consulta_enviar->execute();
+        						$total_ejecutadas++;
+        					}
+        				catch( PDOException $ErrorPDO)
+        					{
+        					    PCOCLI_Mensaje("ERROR DURANTE EJECUCION SQL:");
+        					    echo "\n\r";
+        						echo "SQL EJECUTADO: ".$consulta." ==>> ".$ErrorPDO->getMessage();
+        						$hay_error=1;
+        					}
+        			}
+        
+        	//PASO 2: Verifica las tablas creadas en la base de datos
+                PCOCLI_Mensaje("RESUMEN DE OPERACIONES MOTOR: ".$MotorEvaluado);
+        		$resultado=PCO_ConsultarTablas();
+                $total_tablas=0;
+        		while ($registro = $resultado->fetch())
+        			{
+        				$total_registros=PCO_ContarRegistrosTabla($registro["0"]);
+        				echo "\n\r";
+        				echo 'Tabla: '.$registro[0].'='.$total_registros.' registros. ';
+                        $total_tablas++;
+        			}
+                echo '  TOTAL TABLAS: '.$total_tablas;
+        
+        	if (@$hay_error==1)
+        		$estado_final=1;
+        }
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	if ($hay_error)
 		{
             echo ">>> Se encontraron ERRORES al ejecutar: ";
