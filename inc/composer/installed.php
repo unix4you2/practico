fa650f10808d4e70e00f60ae4fee25c69f91c49b
unix4you2 <unix4you2@gmail.com>
2022-11-03 18:18:17 +0000

    D3 JS como dependencia

--- inc/composer/installed.php
+++ inc/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'practico/framework',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => 'ed85f302304680e6e80f47b3684ee3f2ffc1ed9f',
+        'reference' => '7aa2b81518098bf550086083d78773b23ba17c20',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -19,6 +19,15 @@
             'aliases' => array(),
             'dev_requirement' => false,
         ),
+        'd3js/d3js' => array(
+            'pretty_version' => '7.6.1',
+            'version' => '7.6.1.0',
+            'reference' => 'tags/v7.6.1/',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../d3js/d3js',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
         'jbdemonte/barcode' => array(
             'pretty_version' => '2.0.3',
             'version' => '2.0.3.0',
@@ -31,7 +40,7 @@
         'practico/framework' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => 'ed85f302304680e6e80f47b3684ee3f2ffc1ed9f',
+            'reference' => '7aa2b81518098bf550086083d78773b23ba17c20',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
