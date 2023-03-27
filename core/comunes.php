bf1ee549167197d91425eb85fea7f94b0567ea8a
unix4you2 <unix4you2@gmail.com>
2023-03-27 20:23:59 +0000

    Validacion si realmente se ha obtenido un error

--- core/comunes.php
+++ core/comunes.php
@@ -2624,12 +2624,15 @@ function PCO_ManejadorExcepciones($DetalleExcepcion)
         if ($ModoDepuracion)
             {
                 $Detalles=error_get_last();
-                $Tipo=@$Detalles["type"];
-                $Mensaje=@$Detalles["message"];
-                $Archivo=@$Detalles["file"];
-                $Linea=@$Detalles["line"];
-                if ($Archivo!="" && $Mensaje!="")
-                    PCO_Mensaje($MULTILANG_Atencion." (PHP Exception cod $Tipo)","$Archivo (linea $Linea)<br>$Mensaje", '', 'fa fa-exclamation-triangle texto-rojo texto-blink', 'alert alert-warning');
+                if ($Detalles!=null)
+                    {
+                        $Tipo=@$Detalles["type"];
+                        $Mensaje=@$Detalles["message"];
+                        $Archivo=@$Detalles["file"];
+                        $Linea=@$Detalles["line"];
+                        if ($Archivo!="" && $Mensaje!="")
+                            PCO_Mensaje($MULTILANG_Atencion." (PHP Exception cod $Tipo)","$Archivo (linea $Linea)<br>$Mensaje", '', 'fa fa-exclamation-triangle texto-rojo texto-blink', 'alert alert-warning');
+                    }
             }
     }
 
@@ -2649,12 +2652,15 @@ function PCO_ManejadorErrores($DetalleExcepcion)
         if ($ModoDepuracion)
             {
                 $Detalles=error_get_last();
-                $Tipo=@$Detalles["type"];
-                $Mensaje=@$Detalles["message"];
-                $Archivo=@$Detalles["file"];
-                $Linea=@$Detalles["line"];
-                if ($Archivo!="" && $Mensaje!="")
-                    PCO_Mensaje($MULTILANG_Atencion." (PHP Error cod $Tipo)","$Archivo (linea $Linea)<br>$Mensaje", '', 'fa fa-exclamation-triangle texto-rojo texto-blink', 'alert alert-danger');
+                if ($Detalles!=null)
+                    {
+                        $Tipo=@$Detalles["type"];
+                        $Mensaje=@$Detalles["message"];
+                        $Archivo=@$Detalles["file"];
+                        $Linea=@$Detalles["line"];
+                        if ($Archivo!="" && $Mensaje!="")
+                            PCO_Mensaje($MULTILANG_Atencion." (PHP Error cod $Tipo)","$Archivo (linea $Linea)<br>$Mensaje", '', 'fa fa-exclamation-triangle texto-rojo texto-blink', 'alert alert-danger');
+                    }
             }
     }
 
